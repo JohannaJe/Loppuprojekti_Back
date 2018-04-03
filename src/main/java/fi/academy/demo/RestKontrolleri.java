dff277442ef20c652e2e9404586537fb7af4e388
joniketo <joni.m.keto@gmail.com>
2018-04-03 11:10:20 +0300

    palautettava lista luotu

--- src/main/java/fi/academy/demo/RestKontrolleri.java
+++ src/main/java/fi/academy/demo/RestKontrolleri.java
@@ -7,15 +7,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 public class RestKontrolleri {
 
 
     @GetMapping("/testi")
-    public String haesanonnat() {
-        return "Friedrich Nietsche: Jumala on kuollut, Jumala: Friedrich Nietsche on kuollut, Chuck Norris: Friedrich Nietsche ja Jumala on kuollut, Horst Schimanski: Minä olen Jumala";
-    }
+//    public String haesanonnat() {
+//        return "Friedrich Nietsche: Jumala on kuollut, Jumala: Friedrich Nietsche on kuollut, Chuck Norris: Friedrich Nietsche ja Jumala on kuollut, Horst Schimanski: Minä olen Jumala";
+//    }
 
+    public List<String> naytaLista() {
+        List<String> lista = new ArrayList();
+        lista.add("Friedrich Nietsche: Jumala on kuollut");
+        lista.add("Jumala: Friedrich Nietsche on kuollut");
+        lista.add("Chuck Norris: Friedrich Nietsche ja Jumala on kuollut");
+        lista.add("Horst Schimanski: Minä olen Jumala");
+        return lista;
+    }
 
 }
