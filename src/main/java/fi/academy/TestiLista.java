f17ce33934a04580c7a510ab3cd31cafa9947909
joniketo <joni.m.keto@gmail.com>
2018-04-03 10:32:26 +0300

    testilista muokattu

--- src/main/java/fi/academy/TestiLista.java
+++ src/main/java/fi/academy/TestiLista.java
@@ -1,10 +1,12 @@
 package fi.academy;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class TestiLista {
 
-    List<String> quote = ["Friedrich Nietsche: Jumala on kuollut", "Jumala: Friedrich Nietsche on kuollut", "Chuck Norris: Friedrich Nietsche ja Jumala on kuollut",
-    "Horst Schimanski: Minä olen Jumala"];
+    List<String> testiLista = Arrays.asList("Friedrich Nietsche: Jumala on kuollut", "Jumala: Friedrich Nietsche on kuollut", "Chuck Norris: Friedrich Nietsche ja Jumala on kuollut",
+    "Horst Schimanski: Minä olen Jumala");
 
 }
