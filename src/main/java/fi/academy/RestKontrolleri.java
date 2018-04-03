419d01c689234f4907b95b00f8c4bc0b0ca89c41
nikisem <niki.semenkov@gmail.com>
2018-04-03 10:33:51 +0300

    Konfiguroitu RestKontrolleri.

--- src/main/java/fi/academy/RestKontrolleri.java
+++ src/main/java/fi/academy/RestKontrolleri.java
@@ -1,4 +1,26 @@
 package fi.academy;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class RestKontrolleri {
+
+    private TestiRepository testiRepository;
+
+    public RestKontrolleri(@Autowired TestiRepository testiRepository) {
+        this.testiRepository = testiRepository;
+    }
+
+    @GetMapping("/testi")
+    public Iterable<TestiLista> haesanonnat(SpringDataWebProperties.Pageable pageable) {
+        return testiRepository.findAll();
+    }
+
+
 }
