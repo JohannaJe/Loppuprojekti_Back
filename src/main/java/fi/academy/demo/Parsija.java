e9bcb3278fbc5e1e892224b6a59a40f40caaa1a4
nikisem <niki.semenkov@gmail.com>
2018-04-04 15:10:26 +0300

    Poistettu lukija. Poistettu turhaa koodia.

--- src/main/java/fi/academy/demo/Parsija.java
+++ src/main/java/fi/academy/demo/Parsija.java
@@ -15,46 +15,43 @@ import org.w3c.dom.Element;
 public class Parsija {
 
 
-
     public List parsiFeedit(String urli) {
         List<Feedi> feediLista = new ArrayList<>();
 
         try {
 
 
-            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-            DocumentBuilder db = dbf.newDocumentBuilder();
-//            Document doc = db.parse(new URL(urli).openStream());
-
-
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
             Document doc = dBuilder.parse(urli);
             doc.getDocumentElement().normalize();
-            System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
             NodeList nList = doc.getElementsByTagName("item");
-            System.out.println("----------------------------");
 
             for (int temp = 0; temp < nList.getLength(); temp++) {
                 Node nNode = nList.item(temp);
-                System.out.println("\nCurrent Element :" + nNode.getNodeName());
 
                 if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
                     Element eElement = (Element) nNode;
+
                     String otsikko = eElement
                             .getElementsByTagName("title")
                             .item(0)
                             .getTextContent();
+
                     String linkki = eElement
                             .getElementsByTagName("link")
                             .item(0)
                             .getTextContent();
+
                     String pvm = eElement
                             .getElementsByTagName("pubDate")
                             .item(0)
                             .getTextContent();
                     LocalDateTime aikaleima = LocalDateTime.parse(pvm, DateTimeFormatter.RFC_1123_DATE_TIME);
+
                     String kuva;
+
                     try {
                         kuva = eElement
                                 .getElementsByTagName("media:content")
@@ -65,34 +62,11 @@ public class Parsija {
                     } catch (NullPointerException npe) {
                         kuva = "https://kuvat.uusisuomi.fi/sites/default/files/imagecache/artikkelikuva_std/kuvat/iso_karhu_125l.jpg";
                     }
-                      //  kuva = "https://kuvat.uusisuomi.fi/sites/default/files/imagecache/artikkelikuva_std/kuvat/iso_karhu_125l.jpg";
 
 
                     feediLista.add(new Feedi(otsikko, linkki, aikaleima, kuva));
 
 
-//                    System.out.println("Student roll no : "
-//                            + eElement.getAttribute("rollno"));
-//                    System.out.println("First Name : "
-//                            + eElement
-//                            .getElementsByTagName("item")
-//                            .item(0)
-//                            .getTextContent());
-//                    System.out.println("Last Name : "
-//                            + eElement
-//                            .getElementsByTagName("title")
-//                            .item(0)
-//                            .getTextContent());
-//                    System.out.println("Nick Name : "
-//                            + eElement
-//                            .getElementsByTagName("nickname")
-//                            .item(0)
-//                            .getTextContent());
-//                    System.out.println("Marks : "
-//                            + eElement
-//                            .getElementsByTagName("marks")
-//                            .item(0)
-//                            .getTextContent());
                 }
             }
         } catch (Exception e) {
